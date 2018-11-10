ae891d2985b2b8b9c134fabe008c8f03b7396aa2
Jakub Syrek Mac <jakubvonsyrek@gmail.com>
2018-11-10 22:37:12 +0100

    Corrected division

--- Powershell/cSharpClassesInPowershell.ps1
+++ Powershell/cSharpClassesInPowershell.ps1
@@ -1,13 +1,13 @@
-$Calculator= @"
+$Calculator = @"
 public class Calc
 {
 public int Add(int a,int b)
 {
 return a+b;
 }
-public float Div(float a,float b)
-{
-return a/b;
+public  decimal Div(decimal a,decimal b)
+{   
+return a/b ;
 }
 public int Mul(int a,int b)
 {
@@ -21,10 +21,10 @@ Add-Type -TypeDefinition $Calculator  ; #PS way
 $CalcInstance = [Calc]::new(); #CSharp way
 
 $CalcInstancePS = New-Object -TypeName Calc ;
-$CalcInstance.Add(20,30) ;
-$CalcInstance.Mul(5,5) ;
-$CalcInstance.Div(20,4) ;
+$CalcInstance.Add(20, 30) ;
+$CalcInstance.Mul(5, 5) ;
+$CalcInstance.Div(20, 4) ;
 
-$CalcInstancePS.Add(20,30) ;
-$CalcInstancePS.Mul(5,5) ;
-$CalcInstancePS.Div(1,4) ;
+$CalcInstancePS.Add(20, 30) ;
+$CalcInstancePS.Mul(5, 5) ;
+$CalcInstancePS.Div(1.0, 4.0) ;
