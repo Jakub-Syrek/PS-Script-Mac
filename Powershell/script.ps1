d9c64a2cc8003e284f0a04aabd34d55f20ecdf6a
Jakub Syrek Mac <jakubvonsyrek@gmail.com>
2018-11-10 20:21:26 +0100

    PS classes

---
+++ Powershell/script.ps1
@@ -0,0 +1,4 @@
+$val1 = 1 ;
+$val2 = 2 ;
+$val3 = $val1 + val2
+$val3
---
+++ Powershell/test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE   
+    [ database_name . [ schema_name ] . | schema_name . ] table_name   
+    ( { <column_definition> } [ ,...n ] )   
+    AS [ NODE | EDGE ]
+[ ; ] 
